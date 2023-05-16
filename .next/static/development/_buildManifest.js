0a2a2a5c1dd52c8ae955b0a85e514d9b8e1c5575
siddhi vigneshwar <siddhivigneshwar@gmail.com>
2023-05-16 12:55:08 +0530

    changes

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/onboard/createaccount":["static\u002Fchunks\u002Fpages\u002Fonboard\u002Fcreateaccount.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fonboard\u002Fcreateaccount"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
